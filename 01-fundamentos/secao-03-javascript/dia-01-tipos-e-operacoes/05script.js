e857fad8306ff4188310938502c6c1f5d7a807ce
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-09 12:42:49 -0300

    Imprimir uma mensagem além do resultado

---
+++ 01-fundamentos/secao-03-javascript/dia-01-tipos-e-operacoes/05script.js
@@ -0,0 +1,7 @@
+let a = 12;
+let b= 7;
+console.log ('soma:' + (a + b));
+console.log ('subtração:' + (a - b));
+console.log ("multiplicação:" + (a * b));
+console.log ('Divisão:' + (a / b));
+console.log ('Módulo:' + (a % b));

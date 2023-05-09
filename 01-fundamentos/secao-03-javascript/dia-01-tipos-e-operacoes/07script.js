e607565ccfe156af577fd60a37f6494ed65af17b
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-09 13:25:49 -0300

    exercicio if else 2

---
+++ 01-fundamentos/secao-03-javascript/dia-01-tipos-e-operacoes/07script.js
@@ -0,0 +1,9 @@
+let a = 4;
+let b = 33;
+let c = 40;
+
+if(a > b && a > c) {console.log ('O maior número é: ' + a + ' (a)');
+
+} else if (b > a && b > c) {console.log('O maior número é: ' + b + ' (b)');
+
+} else console.log('O maior número é: ' + c + ' (c)');

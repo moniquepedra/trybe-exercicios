c57422371abd4e351eb07af49acf77c56e0d190f
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-09 17:07:11 -0300

    exercicio array

---
+++ 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/script.js
@@ -0,0 +1,7 @@
+let menu = ["home","serviços","portfólio","links"];
+let menuServices = menu[1];
+console.log(menuServices);
+let indexOfPortfolio = menu[2];
+console.log(indexOfPortfolio);
+menu.push ("Contato");
+console.log(menu);

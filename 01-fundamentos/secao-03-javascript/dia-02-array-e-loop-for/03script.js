54e8d88477a89620cdf794ee83c7274a133754ee
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-10 12:56:13 -0300

    Calcula a média artmética dos elementos da array

--- 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/03script.js
+++ 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/03script.js
@@ -1,7 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
-for (let index =0; index < numbers.length; index += 1) {
-    result += numbers[index];
+let sum = 0; // variável "sum" vai conter a soma dos elementos
+for (let index = 0; index <numbers.length; index += 1) {
+    sum +=numbers[index]; // somar na variável "sum", cada elemento do array. Ao final da execução do elemento "for", "sum" terá a soma de todos os elementos do array
 }
-
-console.log (result);
+//em seguida, calcula a média artmetica dos elementos, cria a variável average, que receberá o valor da soma de todos os elementos do array = sum divido pela quantidade de elementos que tem no array
+let average = sum / numbers.length;
+console.log (average);

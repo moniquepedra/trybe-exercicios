a16a55512589802ea684774227e9713f01f9098d
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-10 17:18:31 -0300

    encontrar o maior número da lista

--- 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/03script.js
+++ 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/03script.js
@@ -1,14 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
 
-for (let index = 0; index < numbers.length; index += 1) {
-  result += numbers[index];
+// O valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso o valor 101 fosse atribuído à variável, o algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema, inicie a variável com o primeiro valor do array. Assim, é possível pular a posição 0 e iniciar com index = 1.
+
+let higherNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
 }
 
-result = result / numbers.length;
-
-if (result > 20) {
-  console.log('O valor da média aritmética é maior que 20');
-} else {
-  console.log('O valor da média aritmética é menor ou igual a 20');
-}
+console.log(higherNumber);

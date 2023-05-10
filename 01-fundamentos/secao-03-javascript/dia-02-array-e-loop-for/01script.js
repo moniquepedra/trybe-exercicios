3c2c38463959240372145e045798bddaa7987b21
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-09 23:07:23 -0300

    Testando for of

--- 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/01script.js
+++ 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/01script.js
@@ -2,3 +2,6 @@ let groceryList = ["Arroz","Feijão","Alface","Melancia"];
 for (let index = 0; index <groceryList.length; index +=1)
 {console.log(groceryList[index]);
 }
+for (let item of groceryList) {
+    console.log(item);
+}

1c7adab28dd34403c8fba3e96a293824f0267ce1
Monique Pedra <moniquepedrinha@gmail.com>
2023-05-10 13:03:10 -0300

    imprime mensagem

--- 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/03script.js
+++ 01-fundamentos/secao-03-javascript/dia-02-array-e-loop-for/03script.js
@@ -1,8 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0; // variável "sum" vai conter a soma dos elementos
-for (let index = 0; index <numbers.length; index += 1) {
-    sum +=numbers[index]; // somar na variável "sum", cada elemento do array. Ao final da execução do elemento "for", "sum" terá a soma de todos os elementos do array
+let result = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  result += numbers[index];
 }
-//em seguida, calcula a média artmetica dos elementos, cria a variável average, que receberá o valor da soma de todos os elementos do array = sum divido pela quantidade de elementos que tem no array
-let average = sum / numbers.length;
-console.log (average);
+
+result = result / numbers.length;
+
+if (result > 20) {
+  console.log('O valor da média aritmética é maior que 20');
+} else {
+  console.log('O valor da média aritmética é menor ou igual a 20');
+}
